8f4103d4d9a2495338a594830f1616ea373a7fc4
kirarit <davidkirarit@gmail.com>
2021-03-13 22:52:24 +0300

    create customer error handlers

--- controllers/customerController.ts
+++ controllers/customerController.ts
@@ -1,16 +1,22 @@
 import customerServices from "../services/customerServices.ts";
-import { getQuery } from 'https://deno.land/x/oak/helpers.ts'
+import { getQuery } from "https://deno.land/x/oak/helpers.ts";
 import * as log from "https://deno.land/std/log/mod.ts";
 
 await new Promise((resolve) => setTimeout(resolve, 0));
-
 
 export default {
   /**
    * @description Add a new Customer
    */
-  createCustomer: async ({ request, response }: { request: any; response: any },) => {
+  createCustomer: async ({
+    request,
+    response,
+  }: {
+    request: any;
+    response: any;
+  }) => {
     const body = await request.body();
+    const values = await body.value;
     if (!request.hasBody) {
       response.status = 400;
       response.body = {
@@ -19,23 +25,37 @@ export default {
       };
       return;
     }
+    if (!values.customer_type) {
+      response.status = 400;
+      response.body = {
+        success: false,
+        message: "Please select customer type",
+      };
+      return;
+    }
+    if (!values.title) {
+      response.status = 400;
+      response.body = {
+        success: false,
+        message: "Please select title",
+      };
+      return;
+    }
     try {
       const values = await body.value;
 
-      await customerServices.createCustomer(
-        {
-          client_id: values.client_id,
-          customer_type: values.customer_type,
-          title: values.title,
-          first_name: values.first_name,
-          other_name: values.other_name,
-          email: values.email,
-          msisdn: values.msisdn,
-          company_name: values.company_name,
-          customer_display_name: values.customer_display_name,
-          website: values.website
-        }
-      );
+      await customerServices.createCustomer({
+        client_id: values.client_id,
+        customer_type: values.customer_type,
+        title: values.title,
+        first_name: values.first_name,
+        other_name: values.other_name,
+        email: values.email,
+        msisdn: values.msisdn,
+        company_name: values.company_name,
+        customer_display_name: values.customer_display_name,
+        website: values.website,
+      });
       response.body = {
         status: true,
         status_code: 200,
@@ -50,12 +70,16 @@ export default {
     }
   },
 
-
-
   /**
-  * @description Attach billing
-  */
-  createBilling: async ({ request, response }: { request: any; response: any },) => {
+   * @description Attach billing
+   */
+  createBilling: async ({
+    request,
+    response,
+  }: {
+    request: any;
+    response: any;
+  }) => {
     const body = await request.body();
     if (!request.hasBody) {
       response.status = 400;
@@ -68,44 +92,36 @@ export default {
     try {
       const values = await body.value;
 
-      const createMoreInfo = await customerServices.createAddress(
-        {
-          customer_id: values.customer_id,
-          city_town: values.city_town,
-          state_province: values.state_province,
-          country: values.country,
-          street: values.street,
-          street1: values.street1,
-          city_town1: values.city_town1,
-          state_province1: values.state_province1,
-          country1: values.country1,
-          notes: values.notes,
-          tax_info: values.tax_info,
-          payment_method: values.payment_method,
-          delivery_method: values.delivery_method,
-          terms: values.terms,
-          out_of_balance: values.out_of_balance
-        }
-      );
+      const createMoreInfo = await customerServices.createAddress({
+        customer_id: values.customer_id,
+        city_town: values.city_town,
+        state_province: values.state_province,
+        country: values.country,
+        street: values.street,
+        street1: values.street1,
+        city_town1: values.city_town1,
+        state_province1: values.state_province1,
+        country1: values.country1,
+        notes: values.notes,
+        tax_info: values.tax_info,
+        payment_method: values.payment_method,
+        delivery_method: values.delivery_method,
+        terms: values.terms,
+        out_of_balance: values.out_of_balance,
+      });
 
       if (createMoreInfo) {
-        const updateCustomerStatus = await customerServices.updateCustomer(
-          {
-            customer_id: values.customer_id,
-          }
-        );
+        const updateCustomerStatus = await customerServices.updateCustomer({
+          customer_id: values.customer_id,
+        });
         if (updateCustomerStatus) {
           response.body = {
             status: true,
             status_code: 200,
             message: "Successfully saved",
           };
-
         }
-
       }
-
-
     } catch (error) {
       response.status = 400;
       response.body = {
@@ -116,65 +132,65 @@ export default {
   },
 
   /**
- * @description Get all Customers List
- */
+   * @description Get all Customers List
+   */
   getAllCustomers: async (ctx: any) => {
     try {
       // let kw = request.url.searchParams.get('page_number');
       // console.log("bayo", kw)
-      let { page_number, filter_value, page_size, client_id } = getQuery(ctx, { mergeParams: true });
+      let { page_number, filter_value, page_size, client_id } = getQuery(ctx, {
+        mergeParams: true,
+      });
       const total = await customerServices.getPageSizeCustomer({
-        client_id: client_id
+        client_id: client_id,
       });
       if (filter_value == null || filter_value == "") {
-        console.log(page_number, '||| params');
+        console.log(page_number, "||| params");
 
         if (page_number == null) {
-          page_number = "1"
-          page_size = "10"
+          page_number = "1";
+          page_size = "10";
           const offset = (Number(page_number) - 1) * 10;
           const data = await customerServices.getAll({
             client_id: client_id,
             page_size: Number(page_size),
-            offset: Number(offset)
+            offset: Number(offset),
           });
           ctx.response.body = {
             status: true,
             status_code: 200,
             total: total,
-            data: data
+            data: data,
           };
         } else {
           const offset = (Number(page_number) - 1) * 10;
           const data = await customerServices.getAll({
             client_id: client_id,
             page_size: Number(page_size),
-            offset: Number(offset)
+            offset: Number(offset),
           });
 
           ctx.response.body = {
             status: true,
             status_code: 200,
             total: total,
-            data: data
+            data: data,
           };
         }
       } else {
         // console.log(filter_value, '||| params');
 
         const data = await customerServices.getCustomerFilter({
-          filter_value: filter_value
+          filter_value: filter_value,
         });
 
         ctx.response.body = {
           status: true,
           status_code: 200,
           total: total,
-          data: data
+          data: data,
         };
-
       }
-
     } catch (error) {
       ctx.response.status = 400;
       ctx.response.body = {
@@ -184,28 +200,26 @@ export default {
     }
   },
 
-
-
   /**
-  * @description Get One Customer balance
-  */
+   * @description Get One Customer balance
+   */
   getAllCustomerBalance: async (ctx: any) => {
     try {
-
-      let { client_id, startDate, endDate } = getQuery(ctx, { mergeParams: true });
-      console.log(client_id, '||| params');
+      let { client_id, startDate, endDate } = getQuery(ctx, {
+        mergeParams: true,
+      });
+      console.log(client_id, "||| params");
 
       const data = await customerServices.getCustomerBalance({
         client_id: client_id,
         startDate: startDate,
-        endDate: endDate
+        endDate: endDate,
       });
       ctx.response.body = {
         status: true,
         status_code: 200,
-        data: data
+        data: data,
       };
-
     } catch (error) {
       ctx.response.status = 400;
       ctx.response.body = {
@@ -214,16 +228,14 @@ export default {
       };
     }
   },
-
 
   /**
    * @description Get One Customers List
    */
   getAllCustomersInfo: async (ctx: any) => {
     try {
-
       let { customer_id } = getQuery(ctx, { mergeParams: true });
-      console.log(customer_id, '||| params');
+      console.log(customer_id, "||| params");
 
       const data = await customerServices.getCustomerMore({
         customer_id: customer_id,
@@ -231,9 +243,8 @@ export default {
       ctx.response.body = {
         status: true,
         status_code: 200,
-        data: data
+        data: data,
       };
-
     } catch (error) {
       ctx.response.status = 400;
       ctx.response.body = {
@@ -244,9 +255,15 @@ export default {
   },
 
   /**
-    * @description Get Update Customer
-    */
-  updateUserCustomer: async ({ request, response }: { request: any; response: any },) => {
+   * @description Get Update Customer
+   */
+  updateUserCustomer: async ({
+    request,
+    response,
+  }: {
+    request: any;
+    response: any;
+  }) => {
     const body = await request.body();
     if (!request.hasBody) {
       response.status = 400;
@@ -258,21 +275,19 @@ export default {
     }
     try {
       const values = await body.value;
-      await customerServices.updateCustomerAll(
-        {
-          customer_id: values.customer_id,
-          client_id: values.client_id,
-          customer_type: values.customer_type,
-          title: values.title,
-          first_name: values.first_name,
-          other_name: values.other_name,
-          email: values.email,
-          msisdn: values.msisdn,
-          company_name: values.company_name,
-          customer_display_name: values.customer_display_name,
-          website: values.website
-        },
-      );
+      await customerServices.updateCustomerAll({
+        customer_id: values.customer_id,
+        client_id: values.client_id,
+        customer_type: values.customer_type,
+        title: values.title,
+        first_name: values.first_name,
+        other_name: values.other_name,
+        email: values.email,
+        msisdn: values.msisdn,
+        company_name: values.company_name,
+        customer_display_name: values.customer_display_name,
+        website: values.website,
+      });
       response.body = {
         status: true,
         status_code: 200,
@@ -288,9 +303,15 @@ export default {
   },
 
   /**
-    * @description Get Update Customer More Info
-    */
-  updateUserCustomerMore: async ({ request, response }: { request: any; response: any },) => {
+   * @description Get Update Customer More Info
+   */
+  updateUserCustomerMore: async ({
+    request,
+    response,
+  }: {
+    request: any;
+    response: any;
+  }) => {
     const body = await request.body();
     if (!request.hasBody) {
       response.status = 400;
@@ -303,35 +324,31 @@ export default {
     try {
       const values = await body.value;
 
-      const updateMoreInfo = await customerServices.updateCustomerMore(
-        {
-          customer_id: values.customer_id,
-          city_town: values.city_town,
-          state_province: values.state_province,
-          country: values.country,
-          street: values.street,
-          street1: values.street1,
-          city_town1: values.city_town1,
-          state_province1: values.state_province1,
-          country1: values.country1,
-          notes: values.notes,
-          tax_info: values.tax_info,
-          payment_method: values.payment_method,
-          delivery_method: values.delivery_method,
-          terms: values.terms,
-          out_of_balance: values.out_of_balance
-        }
-      );
+      const updateMoreInfo = await customerServices.updateCustomerMore({
+        customer_id: values.customer_id,
+        city_town: values.city_town,
+        state_province: values.state_province,
+        country: values.country,
+        street: values.street,
+        street1: values.street1,
+        city_town1: values.city_town1,
+        state_province1: values.state_province1,
+        country1: values.country1,
+        notes: values.notes,
+        tax_info: values.tax_info,
+        payment_method: values.payment_method,
+        delivery_method: values.delivery_method,
+        terms: values.terms,
+        out_of_balance: values.out_of_balance,
+      });
       if (updateMoreInfo) {
         response.body = {
           status: true,
           status_code: 200,
-          message: "Customer More Info Updated Successfully"
+          message: "Customer More Info Updated Successfully",
         };
-
       }
-    }
-    catch (error) {
+    } catch (error) {
       response.status = 400;
       response.body = {
         success: false,
@@ -339,6 +356,4 @@ export default {
       };
     }
   },
-
-
 };
