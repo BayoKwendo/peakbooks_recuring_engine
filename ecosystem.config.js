894ab14bc7991d5be51f2973da16ea1fb1f6fff6
bayokwendo <kwendobrian96@gmail.com>
2021-03-29 01:54:21 +0300

    OTP

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     apps: [
       {
-        name: "luckyapi",
+        name: "peakinsght_apis",
         script: "server.ts",
         interpreter: "deno",
         interpreterArgs: "run --allow-write --allow-read --allow-net --allow-env --unstable",
